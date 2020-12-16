c03d56308b755024295e8ee53d37e4425079d97d
Ethan Hiltner <hiltnere@umich.edu>
2020-12-16 12:59:00 -0500

    adding raster data and aggregating

--- code/server.R
+++ code/server.R
@@ -28,11 +28,17 @@ piping_plover <- st_read("www/rangedata/piping_plover/piping_plover/piping_plove
 spotted_turtle <- st_read("www/rangedata/spotted_turtle/spotted_turtle/spotted_turtle.shp")
 
 # read in land use data
-#land <- raster("www/landusedata/gaplf2011lc_v30_mi.tif")
+land <- raster("www/landusedata/gaplf2011lc_v30_mi.tif")
 
+#selecting for each land use type
+urbanland <- land %in% 580:584
+agland <- land %in% 555:557
+disturbed <-land %in% 558:576
 
-#urbanland <- land %in% 580:584
-#agland <- land %in% 555:557
+#aggregating land use data to reduce raster file size
+urbanland_agg<- aggregate(urbanland, fact=15)
+agland_agg<- aggregate(agland, fact=15)
+disturbedland_agg<- aggregate(disturbed, fact=15)
 
 
 
